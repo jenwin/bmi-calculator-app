e5e936e46e35c212f7684f43462b037003708a06
jenwin <nguy.jenni@gmail.com>
2020-12-11 21:56:26 -0600

    create responsive.css

--- js/app.js
+++ js/app.js
@@ -1,32 +1,26 @@
 const calcBMI = document.getElementById('bmiButton');
-
 calcBMI.addEventListener('click', () => {
 //height and weight
 const heightFeet = document.getElementById('height-feet').value;
 const heightInches = document.getElementById('height-inches').value;
 const weight = document.getElementById('weight-lbs').value;
-
 const numHeightFeet = Number(heightFeet);
 const numHeightInches = Number(heightInches);
 const numWeight = Number(weight);
-
 //total height in inches
 const addAllHeight = ((numHeightFeet * 12) + numHeightInches);
-
 //total height in meters
 const totalHeight = Math.pow((addAllHeight * 0.0254), 2);
-
 //total weight in kg
 const totalWeight = (numWeight * 0.45);
-
 //total bmi
 const totalBMI = (totalWeight/totalHeight).toFixed(1);
-
 //display total value
 let bmiResults = document.getElementById('bmiresults');
-if (totalBMI.length === 3) { 
+
+if (totalBMI.length === 3) {
   bmiResults.disabled = true;
-} else { 
+} else {
   bmiResults.value = totalBMI;
 }
 
